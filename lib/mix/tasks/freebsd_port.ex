0ca83851c743fe606bc5cbb4f780fed05da1f11a
Pat Maddox <pat@patmaddox.com>
2022-05-13 02:13:46 -0700

    Generate port dist_version from the cirrus build ID and commit sha

---
+++ lib/mix/tasks/freebsd_port.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.FreebsdPort do
+  @moduledoc "Generate FreeBSD port files: `mix help freebsd_port`"
+
+  use Mix.Task
+
+  @shortdoc "Generates FreeBSD port files"
+  def run(_) do
+    result = EEx.eval_file("freebsd/Makefile.eex", assigns: assigns())
+    File.write!("freebsd/Makefile", result)
+    IO.puts("Wrote freebsd/Makefile")
+  end
+
+  defp assigns do
+    %{
+      github_tag: github_tag(),
+      dist_version: dist_version()
+    }
+  end
+
+  defp github_tag, do: System.fetch_env!("CIRRUS_CHANGE_IN_REPO")
+
+  defp dist_version do
+    {:ok, base_version} = Mix.Project.config() |> Keyword.fetch(:version)
+    build_id = System.fetch_env!("CIRRUS_BUILD_ID")
+    "#{base_version}-b#{build_id}"
+  end
+end
