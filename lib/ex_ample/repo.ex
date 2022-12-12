9e66a81dc4c874dec62f162e902af5ef515455b3
Pat Maddox <pat@patmaddox.com>
2022-12-12 00:41:20 -0800

    Configure database URL from env var

--- lib/ex_ample/repo.ex
+++ lib/ex_ample/repo.ex
@@ -2,4 +2,8 @@ defmodule ExAmple.Repo do
   use Ecto.Repo,
     otp_app: :ex_ample,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.fetch_env!("DATABASE_URL"))}
+  end
 end
